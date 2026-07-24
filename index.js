59e370b0106add08f5605fd1ed02b1998924bc1d
manuel perez <manuelperez.0000@gmail.com>
2026-07-24 13:19:27 -0400

    aplicando cors permitidas

--- index.js
+++ index.js
@@ -9,7 +9,28 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 configureWebPush();
 
-app.use(cors());
+const whitelist = [
+  'https://girorides.com',
+  'https://www.girorides.com',
+  'https://girove.vercel.app/'
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Bloqueado por la política de CORS: Origen no autorizado.'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+};
+
+// 3. Aplica el middleware globalmente
+app.use(cors(corsOptions));
+
+
 app.use(express.json());
 // Silenciar las peticiones automáticas de favicon
 app.get(['/favicon.ico', '/favicon.png'], (req, res) => res.status(204).end());
